b1fac4e6d40bba5bf3d57e338f2b6aef44d2eefd
deependra227 <45533969+deependra227@users.noreply.github.com>
2019-06-05 16:25:11 +0530

    Add files via upload

---
+++ 1.1/index_js.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $(".regular").slick({
+          
+          slidesToShow: 3,
+          slidesToScroll: 1
+        });
+        
+  $('.slick-prev').css("background-color", "black");
+  $('.slick-next').css("background-color", "black");
+  });
